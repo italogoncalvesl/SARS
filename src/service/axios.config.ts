105a309887ad7dbc53d29ee65c6ade9adae40a7a
im4nu <victorx811@gmail.com>
2023-11-29 11:29:53 -0300

    Finished

--- src/service/axios.config.ts
+++ src/service/axios.config.ts
@@ -1,3 +1,4 @@
 import axios from 'axios';
 
-export const api = axios.create({ baseURL: 'http://127.0.0.1:5000' });
+// LEMBRE-SE DE ALTERAR O IP PARA O SEU IP LOCAL
+export const api = axios.create({ baseURL: 'http://192.168.0.106:5000' });
